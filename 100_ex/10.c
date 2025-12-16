5ebac99949c79826102bbdd1314340f718da9a4b
Konstantin Sultanbaev <dr_ks@mail.ru>
2025-12-16 17:49:49 +0000

    10.c

---
+++ 100_ex/10.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+	int number;
+	printf("Введите целое число: ");
+	scanf("%d", &number);
+
+	if(number % 2 == 0)
+		printf("%d - четное\n", number);
+	else
+		printf("%d - нечетное\n", number);
+	return 0;
+}
