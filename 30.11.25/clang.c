a514f1e3da697a7f7ed7211fa023680b20019d74
Konstantin Sultanbaev <dr_ks@mail.ru>
2025-12-16 17:40:11 +0000

    From server merge

---
+++ 30.11.25/clang.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main() {
+	/*const double pi = 3.14;
+	double V, r, h;
+	printf("Введите значения r и h через пробел: ");
+	scanf("%lg %lg", &r, &h);
+	V = pi * r * r * h;
+	printf("Объем бассейна равен %lg\n", V);
+	return 0;
+	*/
+	/*int minutes;
+	printf("Enter minutes: ");
+	scanf("%d", &minutes);
+	printf("\n%d минут = %d часов, %d минут\n", minutes, minutes / 60, minutes % 60);
+	*/
+
+	/*
+
+	int A, B, min, max, i;
+	scanf("%d %d", &A, &B);
+	if (A > B){
+		max = A;
+		min = B;
+	} else {
+		max = B;
+		min = A;
+	}
+	for (i = min; i <= max;)
+		printf("%d ", i++);
+	printf("\nОбщее количество = %d\n", max - min +1);
+
+	*/
+
+	int size, i, j;
+	printf("Введите размер стороны: ");
+	scanf("%d", &size);
+	for ()
+
+}
